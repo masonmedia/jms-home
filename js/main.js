57fc9ec50856944612b0c9d86a38fb72ff67249d
Andrew Mason <andrewmasonmedia@gmail.com>
2018-12-20 08:34:50 -0500

    First commit, site files for jms-home

---
+++ js/main.js
@@ -0,0 +1,90 @@
+//navbar auto collapse on click
+//from: https://stackoverflow.com/questions/16680543/hide-twitter-bootstrap-nav-collapse-on-click
+//samuel de backer / 55
+$(function() {
+    $('.nav a').on('click', function(){ 
+        if($('.navbar-toggle').css('display') !='none'){
+            $('.navbar-toggle').trigger( "click" );
+        }
+    });
+});
+
+//Change navbar color on scroll
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+
+//smooth scroll
+
+$(document).ready(function () {
+	// Add smooth scrolling to all links in navbar + footer link
+	$(".navbar a, .contact a, div a, footer a[href='#myPage']").on('click', function (event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+
+			// Prevent default anchor click behavior
+			event.preventDefault();
+
+			// Store hash
+			var hash = this.hash;
+
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 1000, function () {
+
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				//      window.location.hash = hash;
+			});
+		} // End if
+	});
+})
+
+
+//resume first word bold
+//https://stackoverflow.com/questions/55612/css-to-increase-size-of-first-word
+//doesn't work on mobile browswers
+
+//$('#background p, #solo p, #group p, #residencies p').each(function(){
+//    var me = $(this);
+//    me.html( me.text().replace(/(^\w+)/,'<strong>$1 ~</strong>') );
+//  });
+
+
+//lettering .js
+
+$(document).ready(function() {
+	    $(".fancy_title").lettering();
+	  });
+
+// baguette box 
+
+//$(document).ready(function () {
+//	baguetteBox.run('.gallery', {
+//		animation: 'fadeIn',
+//	});
+//});
+
+$(document).ready(function () {
+	baguetteBox.run('.gallery');
+});
+
+
+//img defer load
+
+function init() {
+	var imgDefer = document.getElementsByTagName('img');
+	for (var i = 0; i < imgDefer.length; i++) {
+		if (imgDefer[i].getAttribute('data-src')) {
+			imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
+		}
+	}
+}
+window.onload = init;
